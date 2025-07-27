9b001bd19008fd95308b6100f0230ef6d2130b40
Collin <collincook5804@gmail.com>
2025-07-27 18:29:40 -0500

    Add solution for 557. Reverse Words in a String III

---
+++ easy/557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,13 @@
+                    right --;
+                    hold = s[right];
+                    s[right] = s[left];
+                    s[left] = hold;
+                while(right > left) {
+                    right = i - 1; 
+            if(s[i] == ' ' || i == s.size() - 1){
+        for(int i = 0; i < s.size(); i++) {
+        left = 0;
+                if(i == s.size() - 1) {
+                    right = i;
+                } else {
+                }
