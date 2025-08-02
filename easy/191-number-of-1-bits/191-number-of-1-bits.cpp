db58c893d2c7e39496c4dddf33b69f2d1cf8fe60
Collin <collincook5804@gmail.com>
2025-08-02 14:35:18 -0500

    Add solution for 191. Number of 1 Bits

---
+++ easy/191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <bitset>
+class Solution {
+public:
+    int hammingWeight(int n) {
+        std::bitset<32> binary(n);
+        std::string binString = binary.to_string();
+        return hammingWeight;
+        for(int i = 0; i < binString.size(); i++){
+            if(binString[i] == '1'){
+        }
+        int hammingWeight = 0;
+                hammingWeight ++;
+            }
+    }
+};
